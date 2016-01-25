9618f9de35e8424a90654ec0480412db2b8a233c
stein <stein96@live.nl>
2016-01-25 20:16:31 +0100

    V0.1

---
+++ src/Route.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Szenis;
+
+/**
+ * V0.1 SimpleRouting
+ *
+ * @todo namespace
+ * @todo aditional parameters
+ * @todo find class in the resolver
+ * 
+ * @author Szenis <stein96@live.nl>
+ */
+
+/**
+ * Simple route object
+ */
+class Route
+{
+	/**
+	 * Method of route
+	 *
+	 * @var string
+	 */
+	private $method;
+
+	/**
+	 * Url of route
+	 *
+	 * @var string
+	 */
+	private $url;
+
+	/**
+	 * Classname that the route will use
+	 *
+	 * @var string
+	 */
+	private $class;
+
+	/**
+	 * Function that the route will execute when triggerd
+	 *
+	 * @var string
+	 */
+	private $function;
+
+	/**
+	 * Array with all segments of the url
+	 *
+	 * @var array
+	 */
+	private $segments;
+	
+	/**
+	 * Route constructor
+	 *
+	 * @param string $url
+	 */
+	public function __construct($url)
+	{
+		$this->url = $url;
+		
+		// remove trailing and leading /
+		$requestSegments = trim($url, "/");
+
+		// make an array of all segments
+		$this->segments = explode('/', $requestSegments, PHP_URL_PATH);
+	}
+
+	/**
+	 * Set the method of the current route
+	 *
+	 * @param string $method
+	 */
+	public function setMethod($method)
+	{
+		$this->method = $method;
+	}
+
+	/**
+	 * Get the request method of the current route
+	 *
+	 * @return string
+	 */
+	public function getMethod()
+	{
+		return $this->method;
+	}
+	
+	/**
+	 * Get the url of the current route
+	 *
+	 * @return string
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+	
+	/**
+	 * Set the class name of the current route
+	 *
+	 * @param string $class
+	 */
+	public function setClass($class)
+	{
+		$this->class = $class;
+	}
+	
+	/**
+	 * Get the class name of the current route
+	 *
+	 * @return string
+	 */
+	public function getClass()
+	{
+		return $this->class;
+	}
+	
+	/**
+	 * Set the function name of the route
+	 *
+	 * @param string $function
+	 */
+	public function setFunction($function)
+	{
+		$this->function = $function;
+	}
+	
+	/**
+	 * Get the function name of the route
+	 *
+	 * @return string
+	 */
+	public function getFunction()
+	{
+		return $this->function;
+	}
+
+	/**
+	 * Get segments of current url
+	 *
+	 * @return array
+	 */
+	public function getSegments()
+	{
+		return $this->segments;
+	}
+	
+	/**
+	 * Count the amount of segments in the url
+	 *
+	 * @return int
+	 */
+	public function getSegmentCount()
+	{
+		return count($this->segments);
+	}
+}
---
+++ src/Router.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Szenis;
+
+use Route;
+
+/**
+ * Simple routing collection class
+ */
+class Router
+{
+	/**
+	 * an array with all the registerd routes
+	 *
+	 * @var array
+	 */
+	private $routes;
+
+	/**
+	 * Add new route to routes array
+	 *
+	 * @param array $arguments
+	 */
+	public function add($url, $arguments)
+	{	
+		// if the method is not set throw an exception
+		if (!isset($arguments['method'])) {
+			throw new Exception('Request method argument is missing for route: '.$url);
+		}
+
+		if (!is_array($arguments['method'])) {
+			throw new Exception('Request method should be an array for route: '.$url);
+		}
+		
+		// if no valid url is given throw an exception
+		if ($url === null || $url === '') {
+			throw new Exception('No url provided, for root use /');
+		}
+		
+		// if the class name is not provided throw exception
+		if (!isset($arguments['class'])) {
+			throw new Exception('Class argument is missing');
+		}
+
+		if (!isset($arguments['function'])) {
+			throw new Exception('Function argument is missing');
+		}
+		
+		if (!class_exists($arguments['class'])) {
+			throw new Exception('Class '.$arguments['class'].' not found!');
+		}
+		
+		$classMethods = get_class_methods($arguments['class']);
+		
+		if (!in_array($arguments['function'], $classMethods)) {
+			throw new Exception('Function '.$arguments['function'].' does not exists in class '.$arguments['class']);
+		}
+
+		// create new route object
+		$route = new Route(strtolower($url));
+
+		$route->setMethod($arguments['method']);
+		$route->setClass($arguments['class']);
+		$route->setFunction($arguments['function']);
+
+		$this->routes[] = $route;
+	}
+
+	/**
+	 * Resolve the given url and call the function that belongs to the route
+	 *
+	 * @param  array $request
+	 *
+	 * @return mixed
+	 */
+	public function resolve($request)
+	{
+		foreach ($this->routes as $route) {
+			// remove trailing and leading /
+			$requestSegments = trim(strtolower($request['uri']), "/");
+			// make an array of all segments
+			$requestSegments = explode('/', $requestSegments, PHP_URL_PATH);
+			// count segments
+			$uriSegmentCount = count($requestSegments);
+
+			// check if the route matches the requested method
+			if (!in_array($request['method'], $route->getMethod())) continue;
+
+			// check if the requested uri has the same amount of segments as the provided uri's
+			if ($route->getSegmentCount() !== $uriSegmentCount) continue;
+
+			// get all route segments
+			$routeSegments = $route->getSegments();
+
+			$count = 0;
+			$found = true;
+			$arguments = [];
+
+			// loop over the segments and check if the given segments fit the route
+			foreach ($routeSegments as $segment) {
+				// if the segments are exact the same
+				if ($segment === $requestSegments[$count]) {
+					$count++;
+
+					continue;
+				}
+
+				// if there is a place holder the segment could be an argument
+				if ($this->isPlaceholder($segment)) {
+					$arguments[] = $requestSegments[$count];
+					$count++;	
+
+					continue;
+				}
+
+				continue 2;
+			}
+
+			$className = $route->getClass();
+			$method = new ReflectionMethod($className, $route->getFunction());
+			
+			if (count($arguments) !== $method->getNumberOfParameters()) {
+				throw new Exception('Function '.$route->getFunction().' expects '.$method->getNumberOfParameters().' arguments. '.count($arguments).' arguments given.');
+			}
+
+			return call_user_func_array(array((new $className), $route->getFunction()), $arguments);
+		}
+
+		throw new Exception('404 Route not found');
+	}
+
+	/**
+	 * Check if an url segment is an placeholder
+	 *
+	 * @param  string $segment
+	 *
+	 * @return boolean
+	 */
+	private function isPlaceholder($segment) {
+		return (substr($segment, 0, strlen('{')) === '{' && substr($segment, strlen($segment)-1) === '}');
+	}
+}
