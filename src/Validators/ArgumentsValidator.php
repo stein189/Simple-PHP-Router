8c2a57c181af137f4a6b5ece144ff3648219b500
Stein <stein96@live.nl>
2016-02-02 16:50:29 +0100

    Update ArgumentsValidator.php

--- src/Validators/ArgumentsValidator.php
+++ src/Validators/ArgumentsValidator.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SimpleRouting package.
+ *
+ * (c) Stein Janssen <birdmage@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
 namespace Szenis\Validators;
 
 /**
